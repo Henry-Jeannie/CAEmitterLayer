37a7df35c70e8f1aae05fb53e7afa3a93cea6028
Henry-Jeannie <52474503+Henry-Jeannie@users.noreply.github.com>
2019-07-03 09:32:12 +0800

    Add files via upload

---
+++ CAEmitterLayer/012--CoreAnimation-CAEmitterLayer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  012--CoreAnimation-CAEmitterLayer
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CAEmitterLayer/012--CoreAnimation-CAEmitterLayer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  012--CoreAnimation-CAEmitterLayer
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
